ff31057210241ea081df440bcd6ad572cdf460f5
anast <anastasia@borodulina.me>
2024-12-05 16:35:17 +0700

    yellow circles

---
+++ main.py
@@ -0,0 +1,25 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+import random
+from PyQt6 import uic
+import sys
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        diameter = random.randint(10, 50)
+        x = random.randint(0, self.width() - diameter)
+        y = random.randint(0, self.height() - diameter)
+        self.circles.append((x, y, diameter))
+        self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
