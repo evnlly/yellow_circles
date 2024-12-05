8a117a081ccb522669f8274b0c61411f89744f04
anast <anastasia@borodulina.me>
2024-12-05 18:15:41 +0700

    yellow circles UI 2

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtGui import QPainter, QColor, QPen, QPaintEvent
-from PyQt6.QtCore import QRect
-import random
-from PyQt6 import uic
 import sys
+import random
+from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtGui import QPaintEvent, QPainter, QColor
+from PyQt6.QtCore import QRect
+from PyQt6 import QtWidgets, uic, QtGui, QtCore
 
 
-class MyWidget(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.run)
+        super(MainWindow, self).__init__()
+        uic.loadUi("UI.ui", self)
+        self.pushButton.clicked.connect(self.add_circle)
+
         self.circles = []
 
-    def run(self):
+    def paintEvent(self, event: QPaintEvent):
+        painter = QPainter(self)
+        for circle in self.circles:
+            painter.setBrush(QColor(255, 255, 0))
+            painter.drawEllipse(circle)
+
+    def add_circle(self):
         diameter = random.randint(10, 100)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
@@ -21,16 +28,9 @@ class MyWidget(QMainWindow):
         self.circles.append(circle)
         self.update()
 
-    def paint(self, event: QPaintEvent):
-        painter = QPainter(self)
-        pen = QPen(QColor("yellow"), 2)
-        painter.setPen(pen)
-        for x, y, diameter in self.circles:
-            painter.drawEllipse(x, y, diameter, diameter)
 
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec())
