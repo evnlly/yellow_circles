58142f2c2679d59d615bd032f329845568265e2e
anast <anastasia@borodulina.me>
2024-12-05 18:06:48 +0700

    yellow circles UI

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QPainter, QColor, QPen, QPaintEvent
+from PyQt6.QtCore import QRect
 import random
 from PyQt6 import uic
 import sys
@@ -9,13 +11,22 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.run)
+        self.circles = []
 
     def run(self):
-        diameter = random.randint(10, 50)
+        diameter = random.randint(10, 100)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
-        self.circles.append((x, y, diameter))
+        circle = QRect(x, y, diameter, diameter)
+        self.circles.append(circle)
         self.update()
+
+    def paint(self, event: QPaintEvent):
+        painter = QPainter(self)
+        pen = QPen(QColor("yellow"), 2)
+        painter.setPen(pen)
+        for x, y, diameter in self.circles:
+            painter.drawEllipse(x, y, diameter, diameter)
 
 
 if __name__ == '__main__':
